a9660d7abd61fc208a9eb3ce1911de3d8f636c2c
Rohan Patel <96464493+Apurva20997@users.noreply.github.com>
2022-10-11 11:57:35 +0530

    All three carousel created

---
+++ JAndHWebsite/src/app/Modules/Home/Components/ServicesCarousel/services-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services-carousel',
+  templateUrl: './services-carousel.component.html',
+  styleUrls: ['./services-carousel.component.css']
+})
+export class ServicesCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- JAndHWebsite/src/app/Modules/Home/home.module.ts
+++ JAndHWebsite/src/app/Modules/Home/home.module.ts
@@ -3,15 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { ProductsCarouselComponent } from './Components/ProductsCarousel/products-carousel.component';
+
+import { NzCarouselModule } from 'ng-zorro-antd/carousel';
+import { HomeCarouselComponent } from './Components/HomeCarousel/home-carousel.component';
+import { ServicesCarouselComponent } from './Components/ServicesCarousel/services-carousel.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ProductsCarouselComponent,
+    HomeCarouselComponent,
+    ServicesCarouselComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    NzCarouselModule
   ]
 })
 export class HomeModule { }
