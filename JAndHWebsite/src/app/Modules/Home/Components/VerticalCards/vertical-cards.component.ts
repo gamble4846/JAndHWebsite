421ab2303756101b3756f48755b5b7942674c90d
Rohan Patel <42444158+gamble4846@users.noreply.github.com>
2022-10-24 21:45:12 +0530

    MakeToOrder dynamic Complated

--- JAndHWebsite/src/app/Modules/Home/Components/VerticalCards/vertical-cards.component.ts
+++ JAndHWebsite/src/app/Modules/Home/Components/VerticalCards/vertical-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-vertical-cards',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerticalCardsComponent implements OnInit {
 
+  @Input() makeToOrderData:any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.makeToOrderData);
+  }
+
+  ngOnChanges() {
+    this.ngOnInit();
   }
 
 }
